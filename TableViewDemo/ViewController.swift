7edf9667091c04ea3ca914d95a315681e9e561e0
Sgmedical <sgmedical@sgs-macbook-air.local>
2016-10-18 16:02:14 +0800

    1. Initialize the project with Table view control 2. start up the table view with one section 3 rows

--- TableViewDemo/ViewController.swift
+++ TableViewDemo/ViewController.swift
@@ -8,13 +8,35 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
+    @IBOutlet weak var tableView: UITableView!
+    
+    var avengers = ["Thor", "Iron man","Spider man"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        
     }
 
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cells = UITableViewCell()
+        cells.textLabel?.text = avengers[indexPath.row]
+        return cells
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
